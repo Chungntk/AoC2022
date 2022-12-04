ae3fe8fd2490405e0565eca77fd96f889a0a4594
chungpro <chung.ngo@onemount.com>
2022-12-04 23:03:42 +0700

    aoc22 day4

---
+++ day4/day4_1.py
@@ -0,0 +1,9 @@
+with open("input_day4.txt") as f:
+    s = 0
+    for line in f:
+        p1, p2  = line.split(',')
+        p1_start, p1_end = p1.split('-')
+        p2_start, p2_end = p2.split('-')
+        if (int(p1_start) - int(p2_start))*(int(p1_end) - int(p2_end)) <= 0:
+            s = s + 1
+    print(s)
